53180e41e89639a422e97feda420308d6165e3f1
Ramon <ramon@ramonuribe.com>
2021-03-26 12:01:45 -0700

    finished adding route guard and cleaned up app.module

---
+++ src/app/_modules/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    }),
+    BrowserAnimationsModule
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule,
+    BrowserAnimationsModule
+  ]
+})
+export class SharedModule { }
