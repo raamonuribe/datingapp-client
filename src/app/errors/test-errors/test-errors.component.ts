06a34b8ba5e70cbaa60b90f0cdd5a12afb9f7ce7
Ramon <ramon@ramonuribe.com>
2021-03-29 12:55:25 -0700

    validation and minor error handling

---
+++ src/app/errors/test-errors/test-errors.component.ts
@@ -0,0 +1,62 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-errors',
+  templateUrl: './test-errors.component.html',
+  styleUrls: ['./test-errors.component.css']
+})
+export class TestErrorsComponent implements OnInit {
+
+  basUrl = 'https://localhost:5001/api';
+  validationErrors: string[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get404() {
+    this.http.get(`${this.basUrl}/buggy/not-found`).subscribe(response => {
+      console.log(response)
+    }, error => {
+      console.log(error)
+    });
+  }
+
+  get400() {
+    this.http.get(`${this.basUrl}/buggy/bad-request`).subscribe(response => {
+      console.log(response)
+    }, error => {
+      console.log(error)
+    });
+  }
+
+  get500() {
+    this.http.get(`${this.basUrl}/buggy/server-error`).subscribe(response => {
+      console.log(response)
+    }, error => {
+      console.log(error)
+    });
+  }
+
+  get401() {
+    this.http.get(`${this.basUrl}/buggy/auth`).subscribe(response => {
+      console.log(response)
+    }, error => {
+      console.log(error)
+    });
+  }
+
+  get400ValidationError() {
+    this.http.post(`${this.basUrl}/account/register`, {}).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+      this.validationErrors = error;
+    });
+  }
+
+
+
+}
